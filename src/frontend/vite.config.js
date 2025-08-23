e05c5623bf82e98f4bb28f7038003398c78a337d
Szymon Scholz <raspherdev@pm.me>
2025-08-23 22:12:16 +0200

    Feat: docker-compose debug (#5)

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    envDir: "../../",
+    plugins: [react()],
+    server: {
+        host: true,  // binds to 0.0.0.0
+        port: process.env.FRONTEND_PORT || 5173
+    }
 })
